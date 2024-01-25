d74915ddb8567f8c773b382da0a2bc53b6d5be5d
Seyed Mehdi Setodegun <seyedmehdisetodegun@gmail.com>
2024-01-25 14:08:12 +0330

    project done.

---
+++ Functions.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myProgramV4
+{
+    public class Functions
+    {
+        public string EvenOddDetector(int number) {
+            if (number % 2 == 0) {
+                return "Your Number is EVEN!";
+            }
+            else {
+                return "Your Number is ODD!";
+            }
+        }
+
+        public int[] RectangleProperties(int width, int length) {
+            int perimeter = 2 * (width + length);
+            int area = width * length;
+            int[] result = [perimeter, area];
+            return result;
+        }
+
+        public double[] CricleProperties(int radius)
+        {
+            double perimeter = radius * 2 * 3.14;
+            double area = Math.Sqrt(radius) * 3.14;
+            double[] result = [perimeter, area];
+            return result;
+        }
+
+        public int[] switchNumbers(int number1, int nubmer2)
+        {
+            int[] allnumbers = [nubmer2, number1];
+            return allnumbers;
+        }
+
+        public int[] FourMainOperators(int number1, int number2)
+        {
+            int total = number1 + number2;
+            int minus = number1 - total;
+            int multiply = number1 * number2;
+            int divide = number1 / total;
+            int[] result = [total, minus, multiply, divide];
+            return result;
+        }
+
+        public double Average(double number1, int number2, int number3)
+        {
+            return (number1 + number2 + number3)/3;
+        }
+        
+        public int Age(DateTime birthDay)
+        {
+            return DateTime.Now.Year - birthDay.Year;
+        }
+
+        public double TemperatureToKelvin(int  temperature)
+        {
+            return temperature + 273.15;
+        }
+
+        public string MonthName(int monthNumber)
+        {
+            string result = "";
+            if (monthNumber >= 1 && monthNumber <= 12)
+            {
+                switch (monthNumber)
+                {
+                    case 1:
+                        result = "the month name is: January";
+                        break;
+                    case 2:
+                        result = "the month name is: February";
+                        break;
+                    case 3:
+                        result = "the month name is: March";
+                        break;
+                    case 4:
+                        result = "the month name is: April";
+                        break;
+                    case 5:
+                        result = "the month name is: May";
+                        break;
+                    case 6:
+                        result = "the month name is: June";
+                        break;
+                    case 7:
+                        result = "the month name is: July";
+                        break;
+                    case 8:
+                        result = "the month name is: August";
+                        break;
+                    case 9:
+                        result = "the month name is: September";
+                        break;
+                    case 10:
+                        result = "the month name is: October";
+                        break;
+                    case 11:
+                        result = "the month name is: November";
+                        break;
+                    case 12:
+                        result = "the month name is: December";
+                        break;
+                }
+            }
+            else { result = "the month number you entered is not valid."; }
+            return result;
+        }
+
+        public int[] SortedNumbers(int[] numbers)
+        {
+            Array.Sort(numbers);
+            return numbers;
+        }
+
+        public string FullName(string firstName, string lastName)
+        {
+            return firstName + " " + lastName;
+        }
+
+        public string TextCuter(string text)
+        {
+            text = text.Trim();
+            text = text.Substring(1, text.Length - 2);
+            return text;
+        }
+
+        public string VowelsCuter(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                //a, e, i, o, u
+                if (text[i] != 'a' && text[i] != 'e' && text[i] != 'i' && text[i] != 'o' && text[i] != 'u')
+                {
+                }
+                else
+                {
+                    text = text.Remove(i, 1);
+                }
+            }
+            return text;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿namespace myProgramV4
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Functions myFunc = new Functions();
+
+            Console.WriteLine("enter a nubmer to detect if it is odd or even:");
+            int numberOddEven = Convert.ToInt16((Console.ReadLine()));
+            Console.WriteLine(myFunc.EvenOddDetector(numberOddEven));
+            Console.WriteLine("------------------------------------------------------------------");
+
+
+            Console.WriteLine("enter two number to calculate the perimeter and the area of a Rectangle that size.");
+            Console.Write("enter the width:");
+            int width = Convert.ToInt16((Console.ReadLine()));
+            Console.Write("enter the length:");
+            int length = Convert.ToInt16((Console.ReadLine()));
+            int[] q2result = myFunc.RectangleProperties(width, length);
+            Console.WriteLine($"the perimeter is: {q2result[0]}\nthe area is: {q2result[1]}");
+            Console.WriteLine("------------------------------------------------------------------");
+
+
+            Console.Write("enter a number as the radius of a circle to calculate its properties:");
+            int radius = Convert.ToInt16((Console.ReadLine()));
+            double[] q3result = myFunc.CricleProperties(radius);
+            Console.WriteLine($"the perimeter of the circle is: {q3result[0]}\nthe area of the circle is: {q3result[1]}");
+            Console.WriteLine("Note: \"pi\" in this case was calculated as \"3.14\"");
+            Console.WriteLine("------------------------------------------------------------------");
+
+            Console.WriteLine("enter to number to switch their places.");
+            Console.Write("enter Number one:");
+            int switchNumber1 = Convert.ToInt16((Console.ReadLine()));
+            Console.Write("enter Nubmer two:");
+            int switchNumber2 = Convert.ToInt16((Console.ReadLine()));
+            int[] q4result = myFunc.switchNumbers(switchNumber1, switchNumber2);
+            Console.WriteLine($"Your number now are: {q4result[0]} {q4result[1]}");
+            Console.WriteLine("------------------------------------------------------------------");
+
+
+            Console.WriteLine("enter two nubmers for me so I can do the 4 main math operations on them.");
+            Console.Write("enter Number one:");
+            int operatorNumber1 = Convert.ToInt16((Console.ReadLine()));
+            Console.Write("enter Number two:");
+            int operatorNumber2 = Convert.ToInt16((Console.ReadLine()));
+            int[] q5result = myFunc.FourMainOperators(operatorNumber1, operatorNumber2);
+            Console.WriteLine($"the total of them: {q5result[0]}" +
+                $"\nthe minus: {q5result[1]}" +
+                $"\nthe multiply: {q5result[2]}" +
+                $"\nthe devide: {q5result[3]}");
+            Console.WriteLine("------------------------------------------------------------------");
+
+
+            Console.WriteLine("enter three numbers so i can show you the average of them.");
+            Console.Write("enter number 1: ");
+            double averageNumber1 = Convert.ToInt16((Console.ReadLine()));
+            Console.Write("enter number 2: ");
+            int averageNumber2 = Convert.ToInt16((Console.ReadLine()));
+            Console.Write("enter number 3: ");
+            int averageNumber3 = Convert.ToInt16((Console.ReadLine()));
+            Console.WriteLine($"the aswer is: {myFunc.Average(averageNumber1, averageNumber2, averageNumber3)}");
+            Console.WriteLine("------------------------------------------------------------------");
+
+
+            Console.Write("enter your birth day so i can calculate your age in the format of (dd/MM/YYYY) like (01/01/2001)");
+            DateTime birthDay = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine(myFunc.Age(birthDay));
+            Console.WriteLine("------------------------------------------------------------------");
+
+            Console.Write("give me a centigrade temperature and I will give it back as kelvin: ");
+            int temperature = Convert.ToInt16((Console.ReadLine()));
+            Console.WriteLine($"Temperature in Kelvin is: {myFunc.TemperatureToKelvin(temperature)}");
+            Console.WriteLine("------------------------------------------------------------------");
+
+
+            Console.Write("enter a month number to get the month name: ");
+            int monthNumber = Convert.ToInt16((Console.ReadLine()));
+            Console.WriteLine(myFunc.MonthName(monthNumber));
+            Console.WriteLine("------------------------------------------------------------------");
+
+
+            Console.WriteLine("give me three numbers and I will sort them for you.");
+            int[] numberSort = new int[3]; 
+            Console.Write("enter number 1: ");
+            numberSort[0] = Convert.ToInt16(Console.ReadLine());
+            Console.Write("enter number 2: ");
+            numberSort[1] = Convert.ToInt16(Console.ReadLine());
+            Console.Write("enter number 3: ");
+            numberSort[2] = Convert.ToInt16(Console.ReadLine());
+            numberSort = myFunc.SortedNumbers(numberSort);
+            Console.WriteLine($"the sorted list is: \n{numberSort[0]}\n{numberSort[1]}\n{numberSort[2]}");
+            Console.WriteLine("------------------------------------------------------------------");
+
+
+            Console.WriteLine("enter your name.");
+            Console.Write("enter your first name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("enter your last name: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine($"your full name is: {myFunc.FullName(firstName, lastName)}");
+            Console.WriteLine("------------------------------------------------------------------");
+
+            
+            Console.WriteLine("write a Text and I will cut the first and last letter of it:");
+            string text = Console.ReadLine();
+            Console.WriteLine($"answer is: {myFunc.TextCuter(text)}");
+            Console.WriteLine("------------------------------------------------------------------");
+
+
+            Console.WriteLine("enter a text to remove all Vowels in that text:");
+            string textVowelsCuter = Console.ReadLine();
+            Console.WriteLine($"your new text is:\n{myFunc.VowelsCuter(textVowelsCuter)}");
+
+
+        }
+    }
+}
